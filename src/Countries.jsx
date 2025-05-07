e694d7e1b7e1c139f3bb653fa9efa42022497630
Ajay kumar <caajaykumar40@gmail.com>
2025-05-07 14:37:21 +0530

    Add Countries component and update App files

---
+++ src/Countries.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+
+
+
+const Card = ({name, flag, abbr}) => {
+    return (
+      <div
+        style={{
+             display:"flex",
+             flexDirection:"column",
+             textAlign:'center',
+             alignItems:"center",
+             justifyContent:'center',
+             width:"200px",
+             height:"200px",
+             border:"1px solid #ccc",
+             borderRadius:"10px",
+             gap:"5px",
+             marginTop:"10px",
+
+
+        }}
+      
+      >
+        <img src={flag} style={{width:'100px', height:'100px'}} alt={name}/>
+        <h2>{name}</h2>
+      </div>
+    );
+  };
+
+
+export default function Countries() {
+   const ENDPOINT = "https://xcountries-backend.azurewebsites.net/all";
+   const [apiData, setApiData] = useState([]);
+
+
+    useEffect(() => {
+        fetch(ENDPOINT)
+        .then((response) => response.json())
+        .then((data) => {
+          setApiData(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+}, [])
+
+    return (
+        <>
+           
+
+           <div
+            style={{
+             display:"flex",
+             flexWrap:"wrap",
+             justifyContent:"center",
+             textAlign:"center",
+             gap:"10px"
+             
+
+            }}
+        
+           >
+
+           {
+                apiData.map(({name, flag, abbr}) =>(
+                    <Card  key={abbr} name ={name} flag={flag} />
+                ))
+            }
+           </div>
+          
+
+
+          
+
+        </>
+    );
+}
